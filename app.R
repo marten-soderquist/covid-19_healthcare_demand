779f38ca248c604f998f284c4d8815286fe1ab5c
Mårten Söderquist <marten.soderquist@umu.se>
2020-05-11 23:54:51 +0200

    Code cleanup
    Reformat and removed some comments

--- app.R
+++ app.R
@@ -3,13 +3,6 @@ library(vroom)
 library(tidyverse)
 source("getSelectionList.R")
 
-#regions = read.csv("data/region_data.csv", encoding = "UTF-8")
-# regions = vroom(
-#   "data/metadata/region_data.csv",
-#   delim = ',',
-#   col_types = list(selectedRegion = "c", MunicipalityCode = "c")
-# )
-# selectableRegions = unique(regions$selectedRegion)
 selectableRegions = getSelectionList()
 ages = vroom::vroom(
   "data/metadata/agegroups.csv",
@@ -30,10 +23,6 @@ municipalityMetadata <-
       CountyCode = "_"
     )
   )
-
-### Vroom can read several files and append these into a single table. So,
-### create one file per municipality to reduce memory use or per parameter
-### combination?
 
 textSize = 18
 lineWidth = 1
@@ -62,54 +51,65 @@ dataSets = list(
 )
 
 
-shinyApp(ui <- fluidPage(title = "COVID-19 i Sverige",
-  titlePanel("COVID-19, vårdbehov och dödlighet i Sverige"),
-  # p("Här presenteras data från modellen som beskrivs i ", 
-  #   tags$b("COVID-19 healthcare demand and mortality in Sweden in response to non-pharmaceutical (NPIs) mitigation and suppression scenarios (Rocklöv, et al.), preprint"),
-  #   tags$a(href="https://www.medrxiv.org/content/10.1101/2020.03.20.20039594v3", "[Länk].")),
-  tags$hr(),
-  fluidRow(
-    column(
-      2,
-      selectInput("regionSelect", "", choices = selectableRegions),
-      p("Resultat kan visas för riket, regioner/län eller enskilda kommuner."),p(tags$em("Rutan är sökbar genom att markera och skriva.")),
-      
-      selectInput(
-        "scenarioSelect",
-        "",
-        choices = c(
-          "Scenario A" = 1,
-          "Scenario B" = 2,
-          "Scenario C" = 3,
-          "Scenario D" = 4,
-          "Scenario E" = 5
+shinyApp(
+  ui <- fluidPage(
+    title = "COVID-19 i Sverige",
+    titlePanel("COVID-19, vårdbehov och dödlighet i Sverige"),
+    
+    tags$hr(),
+    
+    fluidRow(
+      column(
+        2,
+        selectInput("regionSelect", "", choices = selectableRegions),
+        p(
+          "Resultat kan visas för riket, regioner/län eller enskilda kommuner."
+        ),
+        p(tags$em(
+          "Rutan är sökbar genom att markera och skriva."
+        )),
+        
+        selectInput(
+          "scenarioSelect",
+          "",
+          choices = c(
+            "Scenario A" = 1,
+            "Scenario B" = 2,
+            "Scenario C" = 3,
+            "Scenario D" = 4,
+            "Scenario E" = 5
+          )
+        ),
+        p("A) inga folkhälsointerventioner (kontrafaktiskt scenario)"),
+        p(
+          "B) lägre fysisk distansering i åldrar 0-59 år, måttligt i åldrar 60+ år"
+        ),
+        p(
+          "C) måttlig fysisk distansering i åldrar 0-59 år, måttligt hög i åldrar 60+ år"
+        ),
+        p(
+          "D) måttlig fysisk distansering i åldrar 0-59 år, hög i åldrar 60+ år, samt förbättrad isolering av sjuka"
+        ),
+        p("E) samma som D), men med ännu mer förbättrad isolering av sjuka."),
+        p(
+          span(
+            "En komplett beskrivning av scenario, modell och antaganden återfinns "
+          ),
+          a(href = "https://www.medrxiv.org/content/10.1101/2020.03.20.20039594v3", "här.")
         )
       ),
-      p("A) inga folkhälsointerventioner (kontrafaktiskt scenario)"),
-      p("B) lägre fysisk distansering i åldrar 0-59 år, måttligt i åldrar 60+ år"),
-      p("C) måttlig fysisk distansering i åldrar 0-59 år, måttligt hög i åldrar 60+ år"),
-      p("D) måttlig fysisk distansering i åldrar 0-59 år, hög i åldrar 60+ år, samt förbättrad isolering av sjuka"),
-      p("E) samma som D), men med ännu mer förbättrad isolering av sjuka."),
-      p(span("En komplett beskrivning av scenario, modell och antaganden återfinns "),
-        a(href="https://www.medrxiv.org/content/10.1101/2020.03.20.20039594v3", "här."))
-    ),
-    column(10,
-           fluidRow(
-             column(6, plotOutput("plotIVAId")),
-             column(6, plotOutput("plotCareloadId"))
-           ),
-           fluidRow(
-             column(6, plotOutput("plotDeathsId")),
-             column(6, plotOutput("plotContagiousTotalId"))
-           ))
-  )
+      column(10,
+             fluidRow(
+               column(6, plotOutput("plotIVAId")),
+               column(6, plotOutput("plotCareloadId"))
+             ),
+             fluidRow(
+               column(6, plotOutput("plotDeathsId")),
+               column(6, plotOutput("plotContagiousTotalId"))
+             ))
+    )
   ),
   server <- function(input, output, session) {
-    # updateSelectInput(session,
-    #                   inputId = "regionSelect",
-    #                   choices = getSelectionList(),
-    #                   selected = NULL)
-    
     plotData <- reactive({
       regionSelection <-
         regions %>% dplyr::filter(selectedRegion == input$regionSelect)
@@ -130,7 +130,6 @@ shinyApp(ui <- fluidPage(title = "COVID-19 i Sverige",
     commonTheme = theme(text = element_text(size = textSize),
                         legend.position = 'bottom')
     
-    # output$tableOutputId <- renderTable({plotData()})
     output$plotDeathsId <- renderPlot({
       ggplot(plotData(), aes(x = Date, y = deaths)) + geom_line(aes(color = Age), size = lineWidth) + stat_summary(
         fun = sum,
@@ -195,11 +194,7 @@ shinyApp(ui <- fluidPage(title = "COVID-19 i Sverige",
             color = "Åldersgrupp"
           ) + commonTheme
       })
-    
-    # output$t1 <- renderPrint({
-    #   date()[1]})
   }
 )
 
-  shinyApp(ui, server)
-  
+shinyApp(ui, server)
